a59c8098ca2e7e09939adbc86262d7da85ceedb6
cjmiller09 <cohenmiller18@gmail.com>
2026-09-03 09:12:06 -0500

    Completed First 2 Iterator Labs

--- u02c_iterators/labs/Lab 05B - ListIterator/ListIteratorTest.java
+++ u02c_iterators/labs/Lab 05B - ListIterator/ListIteratorTest.java
@@ -7,22 +7,32 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.ListIterator;
-import static java.lang.System.*;
 
 public class ListIteratorTest
 {
 	private ArrayList<String> list;
 
 	public ListIteratorTest(String line) {
+		setTest(line);
 	}
 
 	public void setTest(String line) {
+		String[] words = line.split(" ");
+		list = new ArrayList<>(Arrays.asList(words));
 	}
 
 	public void replace(String toRemove, String replaceWith) {
+		ListIterator<String> it = list.listIterator();
+		while(it.hasNext()){
+			String i = it.next();
+			if(i.equals(toRemove)){
+				it.set(replaceWith);
+			}
+		}
+
 	}
 
 	public String toString() {
-		return "";
+		return list.toString();
 	}
 }

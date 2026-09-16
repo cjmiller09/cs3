6619d418efee29ce19fe6af7053c2ca740a30d1b
cjmiller09 <cohenmiller18@gmail.com>
2026-09-16 09:14:14 -0500

    Complete Grid Lab

--- u03_recursion/labs/09c_Blocks/Grid.java
+++ u03_recursion/labs/09c_Blocks/Grid.java
@@ -7,10 +7,11 @@
 public class Grid
 {
    private String[][] grid;
+   private boolean[][] visited;
 
 	public Grid()
 	{
-		this(0,0,new String[0]);
+		setGrid(0,0,new String[0]);
 	}
 
 	public Grid(int rows, int cols, String[] vals)
@@ -21,24 +22,41 @@ public class Grid
 	public void setGrid(int rows, int cols, String[] vals)
 	{
 		grid = new String[rows][cols];
+		visited = new boolean[rows][cols];
 		int i=0;
 		for(int r=0; r<grid.length; r++){
 			for(int c=0; c<grid[r].length; c++){
 				grid[r][c] = vals[i];
+				visited[r][c] = false;
 				i++;
 			}
 		}
 	}
 
-	public int findMax(String val)
-	{
-		int count=-1;
-		return count;
-	}
-
 	private int findMax(int r, int c, String search)
 	{
-		return 0;
+		if(r<0 || r>=grid.length || c<0 || c>=grid[r].length || visited[r][c] || !grid[r][c].equals(search)){
+			return 0;
+		}
+		visited[r][c] = true;
+		return 1 + findMax(r+1, c, search) + findMax(r-1, c, search) + findMax(r, c+1, search) + findMax(r, c-1, search);
+	}
+
+	public int findMax(String search)
+	{
+		int max = 0;
+		for(int r=0; r<grid.length; r++){
+			for(int c=0; c<grid[r].length; c++){
+				if(grid[r][c].equals(search) && !visited[r][c]){
+					visited[r][c] = true;
+					int count = 1 + findMax(r+1, c, search) + findMax(r-1, c, search) + findMax(r, c+1, search) + findMax(r, c-1, search);
+					if(count > max){
+						max = count;
+					}
+				}
+			}
+		}
+		return max;
 	}
 
 	public String toString()
--- u03_recursion/labs/09c_Blocks/Lab09c.java
+++ u03_recursion/labs/09c_Blocks/Lab09c.java
@@ -10,7 +10,17 @@ public class Lab09c
 {
 	public static void main( String args[] ) throws IOException
 	{
-		Grid test = new Grid(4,4,new String[]{"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"});
+		String[] vowels = {"A", "E", "I", "O", "U"};
+		String[] vals = new String[144];
+		for(int i=0; i<vals.length; i++){
+			vals[i] = vowels[(int)(Math.random()*vowels.length)];
+		}
+		Grid test = new Grid(12,12,vals);
 		System.out.println(test);
+		System.out.println("A count = " + test.findMax("A"));
+		System.out.println("E count = " + test.findMax("E"));
+		System.out.println("I count = " + test.findMax("I"));
+		System.out.println("O count = " + test.findMax("O"));
+		System.out.println("U count = " + test.findMax("U"));
 	}
 }
